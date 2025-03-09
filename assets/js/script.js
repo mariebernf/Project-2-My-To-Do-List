12e30151b21175cd47fa274ef15800dfeff63ef7
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-03-09 20:40:47 +0100

    Select elements and set task count

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/*
+Select elements and set task count.
+*/
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const tasksContainer = document.querySelector("#tasks");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
+
+let taskCount = 0;
+
+const displayCount = ()=>{
+    countValue.innerText = taskCount;
+};
